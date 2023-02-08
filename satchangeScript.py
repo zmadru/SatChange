81be3f8c39acb23e750c7a56eff0a307234f1dd7
Diego Madruga <madru4042@gmail.com>
2023-02-08 11:49:54 +0100

    shortcuts deleted

---
+++ satchangeScript.py
@@ -0,0 +1,13 @@
+import lib.indexes as indexes
+import lib.stackIntFiles as stackIntFiles
+import lib.interpolacion as interpolacion
+import lib.ACF as ACF
+import lib.changeDetector as changeDetector
+
+
+
+if __name__ == '__main__':
+    print('Seleccionar carpeta de imagenes')
+    
+
+
